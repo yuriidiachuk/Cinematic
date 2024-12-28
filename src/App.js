e755447e91fbb7e4ca4968879e2eae614af2fcb1
yurdia817@gmail.com <yurdia817@gmail.com>
2024-12-28 11:36:16 +0100

    debugging error in https request

--- src/App.js
+++ src/App.js
@@ -208,7 +208,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?i=${selectedId}&apikey=${KEY}`
+          `https://www.omdbapi.com/?i=${selectedId}&apikey=${KEY}`
         );
         const data = await res.json();
         setMovie(data);
