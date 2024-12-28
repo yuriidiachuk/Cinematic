7385dfd714cffa3b43afb120a65c9cc8aa81f4af
yurdia817@gmail.com <yurdia817@gmail.com>
2024-12-28 11:21:44 +0100

    debugging errors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import StarRating from "./StarRating";
-
-// function Test() {
-//   const [movieRating, setMovieRating] = useState(0);
-//   return (
-//     <div>
-//       <StarRating
-//         maxRating={10}
-//         color="blue"
-//         size={30}
-//         defaultRating={1}
-//         onSetRating={setMovieRating}
-//       />
-//       <p>This film was rated {movieRating} star</p>
-//     </div>
-//   );
-// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
